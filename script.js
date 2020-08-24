40a89958372ace8c26f6728129229b0c8347c6fa
caupare <67455200+caupare@users.noreply.github.com>
2020-08-24 17:48:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,53 +1,48 @@
-let url = "https://davids-restaurant.herokuapp.com/menu_items.json"
+let url = "https://davids-restaurant.herokuapp.com/menu_items.json";
 
-let menu_items = null;
+let data = null;
+let buyItemList = [];
 
 $("document").ready(function(){
-    $.get(url,function(data, status){
-        if (status == "success"){
-            menu_items = data.menu_items;
-            for (const key in data.menu_items) {
-                let opt = document.createElement("option");
-                opt.textContent = data.menu_items[key].name;
-                opt.value = key; 
+    $.get(url,function(jsonObj,success){
+        data = jsonObj.menu_items;
+
+        for (const item in data) {
+                let temp = data[item].name.toLowerCase();
+                let opt=document.createElement("option");
+                opt.textContent=temp;
+                opt.value=item;
                 document.querySelector('#menu').appendChild(opt);
+    
             }
-        }
-       
     });
-    
-document.querySelector("#menu").addEventListener("change",showdetails);
 
-function showdetails(e){
-    let index = e.target.value;
-    if(menu_items != null){
-        let id = menu_items[index];
-        let pricesmall;
+
+    document.querySelector("#menu").addEventListener("change",show);
+
+    function show() {
         
-        if(id.price_small != null){
-            pricesmall = id.price_small;
+        document.querySelector('#table').innerHTML="";
+        let item=document.querySelector('#menu').value;
+        let list=data[item];
+        for (value in list) {
+            let tr=document.createElement("tr");
+            let thsr=document.createElement("th");
+            thsr.textContent=value;
+            tr.appendChild(thsr);
+            let tdsr=document.createElement("td");
+            if(list[value]==null)
+            {
+                tdsr.textContent="NOT AVAILABLE";
             
+            }
+            else{
+            tdsr.textContent=list[value];
+            }
+            tr.appendChild(tdsr);
+            document.querySelector('#table').appendChild(tr);
         }
-        else{
-            pricesmall = "Not available";
-        }
-        let descrp = id.description;
-        if(descrp == ""){
-            descrp = "Description not available";
-        }
-        document.querySelector("#menuname").textContent = id.name;
-        document.querySelector("#id").textContent = id.id;
-        document.querySelector("#sname").textContent = id.short_name;
-        document.querySelector("#description").textContent = descrp;
-        document.querySelector("#psmall").textContent = pricesmall;
-        document.querySelector("#plarge").textContent = id.price_large;
-        document.getElementById("table1").style.display = "block";
     }
-
     
-}
-
-
+    
 });
-
-
